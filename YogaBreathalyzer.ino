9a45b41a217e6b49b9ef462280ae53e161b9b5f0
Bonnie Barrilleaux <bbarrill@ucdavis.edu>
2013-10-09 21:00:27 -0700

    Added more/better comments

--- YogaBreathalyzer.ino
+++ YogaBreathalyzer.ino
@@ -2,22 +2,23 @@
 // Yoga Breathalyzer. Using Arduino, WS2812/Neopixel-style LED strip, and Modern Device wind sensor:http://moderndevice.com/product/wind-sensor/
 
 #include "FastSPI_LED2.h" //library for driving LED strips is here: http://code.google.com/p/fastspi/
-#include <LiquidCrystal.h>
+#include <LiquidCrystal.h> //library for driving LCD screen
 
+//set up LED strip
 #define NUM_LEDS 120
 struct CRGB leds[NUM_LEDS]; //holds RGB values for the LEDs
 
-// set up max and min values for wind and temp sensors (both built into the Modern Device sensor)
+// set up max and min values for wind and temp1 sensors (both built into the Modern Device sensor)
 int windmin;
 int windmax;
 int tempmin;
 int tempmax;
 
-// initialize the library with the numbers of the interface pins
+//for the LCD screen, initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-int sensorPin = 5; // palm temp sensor pin
+int sensorPin = 5; // temp2 sensor pin
 
-// SMOOTHING: Define the number of samples for smoothing the sensor signal for the palm temperature. 
+// SMOOTHING: Define the number of samples for smoothing the sensor signal for the temp2 sensor
 int reading=0;
 const int numReadings = 20;
 int index = 0;                  // the index of the current reading
@@ -36,7 +37,7 @@ void setup() {
   Serial.begin(9600);
   windmin = analogRead(A0); // calibrate wind sensor
   windmax = windmin+300;    //this is a reasonable range....
-  tempmax = analogRead(A2); // calibrate temp sensor
+  tempmax = analogRead(A2); // calibrate temp1 sensor
   tempmin = tempmax - 30;   //this is a reasonable range....
   
    // set up the LCD's number of columns and rows: 
@@ -45,8 +46,9 @@ void setup() {
 
 
 void loop() {
+  //read wind/temp1 sensor and output results to LED strip
   int windSensor = analogRead(A0); // read wind sensor value
-  int tempSensor = analogRead(A2); // read temp sensor value
+  int tempSensor = analogRead(A2); // read temp1 sensor value
   
   if(windSensor<windmin) windSensor=windmin; // compress ends of scale to avoid going out of range
   if(windSensor>windmax) windSensor=windmax;
@@ -55,37 +57,33 @@ void loop() {
   if(tempSensor>tempmax) tempSensor=tempmin;
   
   int dist = map(windSensor, windmin,windmax,0,119); //remap wind value to length of array
-  int colorval = map(tempSensor, tempmin,tempmax,0,250); //remap temp to some reasonable color values
+  int colorval = map(tempSensor, tempmin,tempmax,0,250); //remap temp1 to some reasonable color values
 
   colorFill(colorval,10,dist,leds);     //fill strip with color
-  for (int i=0; i < NUM_LEDS; i++) { //clear array 
+  for (int i=0; i < NUM_LEDS; i++) { //clear array for the next loop
     memset(leds, 0,  NUM_LEDS * sizeof(struct CRGB));
    }
   
-   //Palm temperature sensor:
+   //Read temp2 sensor, convert to C and F, and display results on LCD screen
    reading = analogRead(sensorPin);  
     //time smoothing
     total= total - readings[index];         
     readings[index] = reading;   // put sensor reading into array:  
     total= total + readings[index];   // add the reading to the total:
     average = total / numReadings;   // calculate the average:  
- 
- 
- float voltage = average * 5;
- voltage /= 1024.0; 
- float temperatureC = (voltage - 0.5) * 100 ;  //converting from 10 mv per degree wit 500 mV offset
-                                               //to degrees ((voltage - 500mV) times 100) 
- // now convert to Fahrenheit
- float temperatureF = (temperatureC * 9.0 / 5.0) + 32.0;
- //print out palm temperature on the LCD screen
+    float voltage = average * 5;  //Convert to C and F 
+    voltage /= 1024.0; 
+    float temperatureC = (voltage - 0.5) * 100 ;  
+    float temperatureF = (temperatureC * 9.0 / 5.0) + 32.0;
+   //print out temp2 reading on the LCD screen, but don't update every cycle to avoid screen flicker
   if(index==0)
-  {
-  lcd.setCursor(0, 0);
-  lcd.print(temperatureC);lcd.print(" C    ");
-  lcd.setCursor(0, 1);
-  lcd.print(temperatureF);lcd.print(" F    ");
-  }
-    index++;
+   {
+    lcd.setCursor(0, 0);
+    lcd.print(temperatureC);lcd.print(" C    ");
+    lcd.setCursor(0, 1);
+    lcd.print(temperatureF);lcd.print(" F    ");
+   }
+   index++;
    if(index>=numReadings) index=0;
 
 }
@@ -121,6 +119,6 @@ CRGB Wheel(uint16_t WheelPos) {
       r = 0;                    // red off
       break;
    }
-  return(CRGB(r, g, b));    
+  return(CRGB(r, g, b));    //my led strip actually has the colors in g, r, b order; may be a nonstandard strip?
 }
 
